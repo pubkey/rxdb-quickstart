2a6daece2700db451594de29dd12ce2ebebf076f
pubkey <8926560+pubkey@users.noreply.github.com>
2023-11-30 03:57:32 +0100

    FIX make editable

--- src/index.ts
+++ src/index.ts
@@ -12,24 +12,19 @@ import { RxTodoDocument } from './schemas/todo.schema.js';
 async function start() {
     console.log('# START');
     const database = await getDatabase();
-    console.log('# database created');
     await addEventHandlers();
-    console.log('# added event handlers');
 
 
-    const $todoList = ensureNotFalsy(document.getElementById('todo-list')) as HTMLUListElement;
+    // render todo list
+    const $todoList = ensureNotFalsy(document.getElementById('todo-list'));
     database.todos.find({
         sort: [
-            { state: 'desc' }
+            { state: 'desc' },
+            { lastChange: 'desc' }
         ]
     }).$.subscribe(todos => {
-        console.log('emitted todos:');
-        console.dir(todos);
         $todoList.innerHTML = '';
-        todos.forEach(todo => {
-            const $el = getHtmlByTodo(todo);
-            $todoList.append($el);
-        });
+        todos.forEach(todo => $todoList.append(getHtmlByTodo(todo)));
     });
 }
 
@@ -40,23 +35,19 @@ async function addEventHandlers() {
     // add todo
     const $insertInput = ensureNotFalsy(document.getElementById('insert-todo')) as HTMLInputElement;
     $insertInput.onkeydown = async (event) => {
-        if (event.code === 'Enter') {
-            const value = $insertInput.value;
-            console.log('add todo ' + value);
+        if (
+            event.code === 'Enter' &&
+            $insertInput.value.length > 0
+        ) {
             await database.todos.insert({
                 id: randomCouchString(10),
-                name: value,
+                name: $insertInput.value,
                 state: 'open',
                 lastChange: Date.now()
             });
             $insertInput.value = '';
         }
     }
-
-    // toggle state
-    window.addEventListener('click', () => {
-
-    });
 
     // clear completed
     const $clearCompletedButton = ensureNotFalsy(document.getElementById('clear-completed')) as HTMLButtonElement;
@@ -84,10 +75,17 @@ function getHtmlByTodo(todo: RxTodoDocument) {
     $checkbox.classList.add('toggle');
     $checkbox.onclick = () => {
         const newState = todo.state === 'done' ? 'open' : 'done';
-        todo.incrementalPatch({ state: newState });
+        todo.incrementalPatch({ state: newState, lastChange: Date.now() });
     }
 
     const $label = document.createElement('label');
+    $label.contentEditable = 'true';
+    $label.onkeyup = (ev) => {
+        if (ev.code === 'Enter') {
+            const newName = $label.innerHTML.replace(/<br>/g, '').replace(/\&nbsp;/g, ' ').trim();
+            todo.incrementalPatch({ name: newName, lastChange: Date.now() });
+        }
+    }
     $viewDiv.append($label);
     $label.innerHTML = escapeForHTML(todo.name);
 
@@ -100,15 +98,6 @@ function getHtmlByTodo(todo: RxTodoDocument) {
         $checkbox.checked = true;
         $liElement.classList.add('completed');
     }
-
-    // $todoList.innerHTML += `
-    // <li data-id="${todo.id}"${completed ? ' class="completed"' : ''}>
-    //     <div class="view">
-    //         <input class="toggle" type="checkbox" ${completed ? 'checked' : ''}>
-    //         <label>${escapeForHTML(todo.name)}</label>
-    //         <button class="destroy"></button>
-    //     </div>
-    // </li>`
 
     return $liElement;
 }
